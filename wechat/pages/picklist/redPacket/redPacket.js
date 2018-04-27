3bc7ff8b3398853edd951db7c4c8a9107749a64a
CallmeJay <291977464@qq.com>
2018-04-27 20:08:41 +0800

    视频和红包

--- wechat/pages/picklist/redPacket/redPacket.js
+++ wechat/pages/picklist/redPacket/redPacket.js
@@ -1,14 +1,74 @@
 // pages/picklist/surveyList/surveyList.js
+var config = require('../../../config');
+
+var timer; // 计时器
+var isOpenRedPacket;
 Page({
   /**
    * 页面的初始数据
    */
-  data: {},
+  data: {
+    timeLeft: 10,  // 倒计时
+    uid: '',  // 用户id
+    orderNo: '', // 订单号,
+    isOpen: false, // 红包拆开了吗
+    isTiming: true, // 是否在倒计时
+  },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function(options) {},
+  onLoad: function(options) {
+
+    // 清除定时器
+    if(timer) clearTimeout(timer);
+
+    // 开始倒计时
+    timer = setInterval (() => {
+      console.log(this.data.timeLeft)
+      if(this.data.timeLeft > 0) {
+        this.setData({
+          timeLeft: --this.data.timeLeft
+        })
+      }else {
+        this.setData({
+          timeLeft: 0,
+          isTiming: false
+        })
+        clearTimeout(timer);
+      }
+    },1000)
+  },
+
+  // 拆开红包
+  openRedPacket() {
+    this.setData({
+      isOpen: true
+    })
+    wx.request({
+      url: config.RequestAddressPrefix5 + '/rider/income/v1/redpacket/receive',
+      method: 'POST',
+      data: {
+        uid: this.data.orderNo,
+        orderNo: this.data.orderNo
+      },
+      success (res) {
+        if(res.data.rescode == 200) {
+          console.log("获取到的红包金额是",res.data.data);
+          isOpenRedPacket = true;
+        }
+      }
+    })
+  },
+
+  // 抢单
+  getMoreOrder () {
+    if(!this.data.timeLeft) {
+      wx.navigateTo({
+        url: '../surveyList/surveyList'
+      })
+    }
+  },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
@@ -18,7 +78,9 @@ Page({
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function() {},
+  onShow: function() {
+    console.log(isOpenRedPacket)
+  },
 
   /**
    * 生命周期函数--监听页面隐藏
